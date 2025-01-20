dd05bf6bb00a46c9ed4fa05bec977bd6d966a491
Kazuu <diegotello07@gmail.com>
2025-01-20 11:49:20 -0500

    sns

---
+++ code.js
@@ -0,0 +1,8 @@
+const boton = document.getElementById("mobButton");
+const parrafo = document.getElementById("parrafoM");
+parrafo.classList.toggle('active');
+//boton.classList.toggle('active');
+boton.addEventListener('click', () => {
+    parrafo.classList.toggle('active');
+    alert("efectivamente click hermanocow")
+});
