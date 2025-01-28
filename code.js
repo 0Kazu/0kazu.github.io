0ac8562c15e87e63bb978b1148934540f1e5462a
Kazuu <diegotello07@gmail.com>
2025-01-28 12:22:55 -0500

    sotellmedoyuuwannago

--- code.js
+++ code.js
@@ -1,7 +1,17 @@
 const boton = document.getElementById("mobButton");
+const boton1 = document.getElementById("mobButton1");
+
 const parrafo = document.getElementById("parrafoM");
+const parrafo1 = document.getElementById("parrafoM1");
+
 parrafo.classList.toggle('active');
+parrafo1.classList.toggle('active');
+
 //boton.classList.toggle('active');
 boton.addEventListener('click', () => {
     parrafo.classList.toggle('active');
+});
+
+boton1.addEventListener('click', () => {
+    parrafo1.classList.toggle('active');
 });
